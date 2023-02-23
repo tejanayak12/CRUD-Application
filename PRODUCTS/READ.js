44cd5cca4c8558a2cb8d765478a66e8d32d2a3f6
Teja nayak <tejanayak12>
2023-02-23 23:58:23 +0530

    application completed

--- PRODUCTS/READ.js
+++ PRODUCTS/READ.js
@@ -18,3 +18,29 @@ const getallproducts = (request , response) => {
     })
 }
 module.exports = getallproducts;
+
+// creating a id query on get.............
+
+const get_product_id = (request,response) =>{
+    const products_id = request.params.id;
+
+    const sql_query = `SELECT * FROM products WHERE products_id = ${products_id}`;
+
+    db.serialize(() => {
+        db.get(sql_query,(error , rows)=>{
+            if (error || !rows) {
+                response.status(404).json({
+                    status : false,
+                    message : `Couldn't find the product id on ${products_id}`
+                })
+            }else{
+                response.json({
+                    status : true,
+                    data : rows
+                })
+            }
+        })
+    })
+};
+
+module.exports = {getallproducts,get_product_id};
