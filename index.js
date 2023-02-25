87ef34277b9aecb32afae213e7854029e8c91699
Teja nayak <tejanayak12>
2023-02-25 20:55:00 +0530

    new data was added

--- index.js
+++ index.js
@@ -25,6 +25,13 @@ app.put(`/products/:id`, update_application);
   
 app.delete("/products/:id" ,deleteproducts);
 
+app.get("/" , (request , response) => {
+    response.json({
+        status : true,
+        api : [products]
+    })
+});
+
 
 app.listen(port ,  () => {
     console.log("sever is running...............")
